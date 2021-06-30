66e2f93c2f2a26816b0bf1808ee1a14200f98b7b
Sebastian Sole <scsole@protonmail.com>
2021-07-01 10:02:53 +1200

    Initial test

---
+++ main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  main.py
+#  
+
+import RPi.GPIO as GPIO
+import time
+import subprocess
+import os
+
+PIR_PIN = 11                    # GPIO17
+GPIO.setmode(GPIO.BOARD)        # Use header pin numbers
+GPIO.setup(PIR_PIN, GPIO.IN)
+
+running = False         # Is a video currently playing?
+player = "omxplayer"    # The video player being used
+video_path = "/home/pi/Videos/trim.mp4" # Path to video
+
+child = 0
+
+if player == "vlc":
+    opt = '--play-and-exit'
+else:
+    opt = ''
+
+try:
+    print("Waiting for motion")
+    
+    while True:
+        if GPIO.input(PIR_PIN):
+            if running == False:
+                print("Motion detected")
+                child = subprocess.Popen([player, video_path, opt])
+                running = True
+                print("Playing video")
+                
+        if running == True:
+            child.poll()
+            if child.returncode == 0:
+                running = False
+                print("Video complete, waiting for motion")
+            time.sleep(1)
+            
+except KeyboardInterrupt:
+    print("Quit")
+    GPIO.cleanup()
