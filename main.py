3fe974ba7443fcb6f4dfb0bd51d92365b84280ba
Sebastian Sole <scsole@protonmail.com>
2021-10-04 14:27:16 +1300

    add path checks

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 #  main.py
 #  
 
+from genericpath import exists
 import RPi.GPIO as GPIO
 import time
 import subprocess
@@ -15,7 +16,9 @@ GPIO.setup(PIR_PIN, GPIO.IN)
 
 running = False         # Is a video currently playing?
 player = "omxplayer"    # The video player being used
-video_path = "/home/pi/video.mp4"   # Path to video file
+primary_video_path = "/mnt/usb/video.mp4"   # Path to primary video file
+secondary_video_path = "/home/pi/video.mp4" # Path to backup video file
+video_path = primary_video_path
 
 child = 0
 
@@ -26,22 +29,30 @@ else:
 
 try:
     print("Waiting for motion")
-    
+
     while True:
         if not GPIO.input(PIR_PIN):
             if running == False:
                 print("Motion detected")
+                if os.path.exists(primary_video_path):
+                    video_path = primary_video_path
+                elif os.path.exists(secondary_video_path):
+                    video_path = secondary_video_path
+                    print("INFO: Using secondary video")
+                else:
+                    print("WARN: Video files not accessible, ignoring motion")
+                    continue
                 child = subprocess.Popen([player, video_path, opt])
                 running = True
                 print("Playing video")
-                
+
         if running == True:
             child.poll()
             if child.returncode == 0:
                 running = False
                 print("Video complete, waiting for motion")
             time.sleep(1)
-            
+
 except KeyboardInterrupt:
     print("Quit")
     GPIO.cleanup()
